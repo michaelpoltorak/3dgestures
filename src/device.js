42c6da4383e8ea28b5c43905fed3742804c406dc
michaelpoltorak <michaelpoltorak@gmail.com>
2015-06-19 11:05:45 +0200

    Now displaying gesture type on screen

--- src/device.js
+++ src/device.js
@@ -1,5 +1,7 @@
 function Device() {
 
+    //The device...
+
     var device = this;
     var listener = null;
     var filters = [];
@@ -9,31 +11,31 @@ function Device() {
     var delay = 1000;
 
     this.start = function () {
-        device.addFilter(new IdleFilter());     //Determines if device is in idle state
-        device.addFilter(new MotionFilter());   //Determines if the device is in motion (based on IdleFilter data)
-        device.addFilter(new DEFilter());       //Determines if the current event differs enough from the previous one
+        device.addFilter(new IdleFilter());     //This filter determines if device is in idle state
+        device.addFilter(new MotionFilter());   //This filter determines if the device is in motion (based on IdleFilter data)
+        device.addFilter(new DEFilter());       //This filter determines if the current event differs enough from the previous one
 
         device.startTrackingOrientation();
     };
 
-    //Tracking orientation
+    //Track the orientation
     this.startTrackingOrientation = function () {
-        //console.log("startTrackingOrientation ", filters)
+        console.log("startTrackingOrientation");
         var vector;
         if (window.DeviceOrientationEvent) {
             // Listen for the deviceorientation event and handle the raw data
             listener = function (eventData) {
                 if (!busy) {
-                    // gamma is the left-to-right tilt in degrees, where right is positive
+                    //gamma: left-to-right tilt in degrees, where right is positive
                     var tiltLeftRight = eventData.gamma;
 
-                    // beta is the front-to-back tilt in degrees, where front is positive
+                    //beta: the front-to-back tilt in degrees, where front is positive
                     var tiltFrontBack = eventData.beta;
 
-                    // alpha is the compass direction the device is facing in degrees
+                    //alpha: the compass direction the device is facing in degrees
                     var direction = eventData.alpha
 
-                    // call our orientation event handler
+                    //call our orientation event handler
                     deviceOrientationHandler(tiltLeftRight, tiltFrontBack, direction);
                 }
             };
@@ -42,30 +44,33 @@ function Device() {
 
         function deviceOrientationHandler(tiltLR, tiltFB, dir) {
             vector = [tiltLR, tiltFB, dir];
-            //console.log("deviceOrientationHandler ", vector);
+
+            //Apply filters
             for (var i = 0; i < filters.length; i++) {
                 vector = filters[i].filter(vector);
             }
+            //If data not filtered away, push data
             if (vector != null) {
                 data.push(vector);
-                if (demo.getType() !== "searchall") {
-                    console.log("deviceOrientationHandler ", vector);
-                }
             }
         }
     }
 
+    //When device movement ends process and send data
+    document.addEventListener("DEVICE_MOTION_STOPPED", function (e) {
+        device.processAndSendData();
+    });
+
+    //Do we have a recognisable gesture?
     this.processAndSendData = function () {
         if (data[0]) {
             var gesture = getGesture();
             if (gesture != null && !busy) {
+                //We have a gesture. Send a message and delay a bit...
+                console.log("gesture: ", gesture);
                 document.dispatchEvent(new CustomEvent("GOT_GESTURE", {
                     "detail": gesture
                 }));
-                if (demo.getType() === "motion") {
-                    ui.message(gesture);
-                }
-                console.log(gesture);
                 busy = true;
                 timer = setTimeout(function () {
                     busy = false;
@@ -75,6 +80,7 @@ function Device() {
             data = [];
         }
 
+        //Do we have a gesture?
         function getGesture() {
             var item_0 = data[0],
                 item_last = data[data.length - 1],
@@ -90,7 +96,8 @@ function Device() {
             if (item_last[1] - item_0[1] > 10) {
                 return "TILT_FORWARD";
             }
-            if (item_last[0] > 30 && ui.touchInProgress) {
+            if (item_last[0] > 30) {
+            //if (item_last[0] > 30 && ui.touchInProgress) {
                 return "TILT_RIGHT_PRESS";
             }
             if (item_last[0] > 30) {
@@ -99,7 +106,6 @@ function Device() {
             if (item_last[0] < -30) {
                 return "TILT_LEFT";
             }
-
             return retval;
         }
     };
@@ -113,7 +119,4 @@ function Device() {
         window.removeEventListener("deviceorientation", listener);
     };
 
-    document.addEventListener("DEVICE_MOTION_STOPPED", function (e) {
-        device.processAndSendData();
-    });
 }
--- src/filters/filters.js
+++ src/filters/filters.js
@@ -1,8 +1,94 @@
-window.filters = (function (window) {
-    console.log("filters init...");
-    return {
-        foo: function () {
-            console.log("foo");
+/**
+ * Determines wether device is moving or not
+ * Returns values above sensitivity threshold
+ **/
+function IdleFilter() {
+
+    this.sensivity = 1;
+
+    /**
+     * @param vector: [x, y, z]
+     **/
+
+    this.filter = function (vector) {
+        if (vector != null) {
+            var absvalue = Math.sqrt((vector[0] * vector[0]) + (vector[1] * vector[1]) + (vector[2] * vector[2]));
+            //document.getElementById("idle").value = (absvalue > 1 + this.sensivity);
+            // Only return vector if above sensitivity
+            if (absvalue > 0.8 + this.sensivity) {
+                //console.log("***************", Math.random(1));
+                return vector;
+            } else {
+                //console.log("...............", Math.random(1));
+                return null;
+            }
         }
     }
-})(window);
+}
+
+function DEFilter() {
+
+    /**
+     * This filter removes a vector, if it doesn't differ enough from
+     * the previously retrieved vector.
+     **/
+
+    this.sensivity = 1;
+    this.reference = [0.0, 0.0, 0.0];
+
+
+    this.filter = function (vector) {
+        //console.log("vector ", vector);
+        var sensivity = this.sensivity;
+        var reference = this.reference;
+        if (vector != null) {
+            if (vector[0] < reference[0] - sensivity ||
+                vector[0] > reference[0] + sensivity ||
+                vector[1] < reference[1] - sensivity ||
+                vector[1] > reference[1] + sensivity ||
+                vector[2] < reference[2] - sensivity ||
+                vector[2] > reference[2] + sensivity) {
+
+                this.reference = vector;
+                //console.log(vector);
+                return vector;
+            } else {
+                return null;
+            }
+        }
+        return null
+    }
+}
+
+
+function MotionFilter() {
+
+    this.nowinmotion = false;
+    this.motionstartstamp = new Date().getMilliseconds();
+    this.motionchangetime = 52
+
+    this.filter = function (vector) {
+        if (this.nowinmotion &&
+            (new Date().getMilliseconds() - this.motionstartstamp) >= this.motionchangetime) {
+            this.nowinmotion = false;
+            document.dispatchEvent(new CustomEvent("DEVICE_MOTION_STOPPED", {
+                "detail": "Device motion ended"
+            }));
+            //console.log("motion stopped");
+        }
+        return (this.foo(vector));
+    }
+
+    this.foo = function (vector) {
+        if (vector != null) {
+            this.motionstartstamp = new Date().getMilliseconds();
+            if (!this.nowinmotion) {
+                this.nowinmotion = true;
+                this.motionstartstamp = new Date().getMilliseconds();
+                //this.device.fireMotionStartEvent();
+                //console.log("motion started");
+            }
+        }
+        return vector;
+    }
+}
